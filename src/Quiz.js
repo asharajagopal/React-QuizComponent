e2c6fb18594a12f5470c4a10c4ca3921038bdd98
Asha Rajagopal <asha.rajagopal@accenture.com>
2019-04-11 01:01:25 +0530

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,14 +1,11 @@
 import React, { Component} from 'react';
-import QuizQuestion from './QuizQuestion.js';
-import QuizEnd from './QuizEnd.js';
+import QuizQuestion from './components/QuizQuestion.js';
+import QuizEnd from './components/QuizEnd.js';
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./data/quiz_data.json');
 
 class Quiz extends Component{
-    constructor (props){
-        super(props)
-        this.state = {quiz_position : 1}
-    }
+    state = {quiz_position : 1}
     showNextQuestion(){
         this.setState((state) =>{
             return{quiz_position : state.quiz_position + 1}
@@ -21,6 +18,9 @@ class Quiz extends Component{
         const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
         return(
         <div>
+            <div className="quiz-header">
+                <h1>Simple Quiz Application</h1>
+            </div>
             {isQuizEnd ? <QuizEnd resetClickHandler= {this.handleResetClick.bind(this)}/> :
             <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} 
             showNextQuestionHandler = {this.showNextQuestion.bind(this)} />}
